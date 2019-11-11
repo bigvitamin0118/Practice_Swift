84b77c2d1d27044f67c2e936b7c989b29126f61f
jp.papaymango <bigvitamin0118@gmail.com>
2019-11-11 11:36:27 +0900

    commit

---
+++ speech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  speech
+//
+//  Created by HT-19R1108 on 2019/11/11.
+//  Copyright © 2019 HT-19R1108. All rights reserved.
+//
+
+import UIKit
+//import <#module#>
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func speech(_ sender: Any) {
+        let msg = "Hello";
+//        let su =
+    }
+    
+}
+
